d92b1b54083d9e9afd5067ff530da5f75601e579
Vasilije Vujovic <vasilijev7@gmail.com>
2022-03-31 11:10:37 +0200

    Vjezba 5, mnozenje decimalnih br.

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+  // only change code below this line
+  product = numOne * numTwo;
+  // only change code above this line
+
+  return product;
+}
+
+console.log(main(2.5, 2)); // Change this line
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.2));
+module.exports = main;
