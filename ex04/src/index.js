cc71054ff97e92fb54b5e816f5f161ffda795733
Vasilije Vujovic <vasilijev7@gmail.com>
2022-03-31 10:55:13 +0200

    Vjezba 4, decimalni brojevi

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+  // only change code below this line
+  var myDecimal = 2.54;
+  var myDecimalE = 2.54e-3;
+  // only change code above this line
+
+  return {
+    myDecimal,
+    myDecimalE,
+  };
+}
+
+console.log(main());
+module.exports = main;
